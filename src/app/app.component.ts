68a68f3de474d6908f4bf27519911f2ba7ee761e
brettwray <brett@brettwray.com>
2018-09-11 20:50:42 -0700

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService} from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stock-search';
+    date:;
+    test: string
+    constructor(private _api: ApiService) {
+    }
+
+    ngOnInit() {
+        this._api.getData()
+            .subscribe((data: any) =>{
+                this.date = data["Time Series (Daily)"]["2018-04-20"]
+
+
+            })
+
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ApiService } from './services/api.service';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './navigation/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { QuoteComponent } from './components/quote/quote.component';
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuoteComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, ChartsModule
   ],
-  providers: [],
+  providers: [ ApiService,  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
